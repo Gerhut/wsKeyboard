7a3c6282f4f13fdd0accc9355ca66db3c6599179
Gerhut <Gerhut@GMail.com>
2013-06-13 21:45:19 +0800

    Test web server updated.
    Move wsKeyboard.js to root directory. Test web server can find files in
    parent directory.

--- test/WebServer.py
+++ test/WebServer.py
@@ -1,23 +1,44 @@
-import SocketServer
+import os
 import socket
+import SocketServer
 import SimpleHTTPServer
 
-def webServer(port=80):
-	Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-	httpd = SocketServer.TCPServer(("", port), Handler)
-	
-	ipList = socket.gethostbyname_ex(socket.gethostname())
-	for ips in ipList:
-	    if type(ips) != list:
-	    	continue
-	    for ip in ips:
-	    	print 'http://%s:%d/' % (ip, port)
-	httpd.serve_forever()
+
+def start_web_server(port=80):
+
+    class ParentDirHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+
+        PARENT_DIR_FILES = set(['wskeyboard.js'])
+
+        def translate_path(self, path):
+            dir(self)
+            path = SimpleHTTPServer.SimpleHTTPRequestHandler.translate_path(self, path)
+            filedir, filename = os.path.split(path)
+            if filename.lower() in self.PARENT_DIR_FILES:
+                return os.path.join(os.path.split(filedir)[0], filename)
+            else:
+                return path
+
+    def get_addresses(address_list=socket.gethostbyname_ex(socket.gethostname())):
+        for address in address_list:
+            if isinstance(address, str):
+                yield address
+            elif isinstance(address, list):
+                for sub_address in get_addresses(address):
+                    yield sub_address
+
+    httpd = SocketServer.TCPServer(('', port), ParentDirHTTPRequestHandler)
+
+    for address in get_addresses():
+        print 'http://%s:%d/' % (address, port)
+
+    httpd.serve_forever()
 
 if __name__ == '__main__':
-	try:
-		port = input('Port: ')
-	except Exception:
-		port = 80
+    DEFAULT_PORT = 80
+    try:
+        port = input('Port(%d): ' % (DEFAULT_PORT,))
+    except SyntaxError:
+        port = DEFAULT_PORT
 
-	webServer(port)
+    start_web_server(port)
